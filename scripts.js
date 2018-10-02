009cfb1f120091721175efca7efad19483e0d520
CullMay <34067482+CullMay@users.noreply.github.com>
2018-10-02 14:16:19 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -33,12 +33,29 @@ for (index in myobject) {
 
 //Display Order Summary Function
 function showOrder() {
+  var num = document.getElementsByName("numPizzas")[0].selectedIndex+1;
+  //var amt = num.options[index];
+ 
+  var pizzaPrice = 10;
+  var taxRate = 0.076;
+
+  var subTotal = num * pizzaPrice;
+  var taxes = subTotal * taxRate;
+  var total = taxes + subTotal;
+
+  //document.write("Blah Blah Blah")
   var name = document.getElementById("fullName");
   var phone = document.getElementById("phone");
   var type = document.getElementsByName("pizzaType")[0];
   var index1 = type.selectedIndex;
   var num = document.getElementsByName("numPizzas")[0];
   var index = num.selectedIndex;
+    
+  document.write("Your Subtotal is:"+ subTotal+" ");
+  document.write("\n");
+  document.write("Your tax is: "+taxes);
+
+  document.write(" Your grand total is:" + total);
 
   //var amt1 = document.getElementsByName("numPizzas")[0];
   //var amt2 = amt1.selectedIndex;
@@ -52,25 +69,8 @@ function showOrder() {
   document.write(type.options[index1].text);
   document.write(" pizza(s) to you ASAP. See you soon!");
     //return amt2;
-}
-
 //Order Calculation For Submit Button -- Still need to finish
-function calcCost() {
-  var num = document.getElementsByName("numPizzas")[0];
-  var index = num.selectedIndex;
-  //var amt = num.options[index];
- 
-  var pizzaPrice = 10;
-  var taxRate = 0.076;
-
-  var subTotal = num * pizzaPrice;
-  var taxes = subTotal * taxRate;
-  var total = taxes + subTotal;
-    
-  document.write(num.options[index].text);
-
-  document.write(total);
 }
 
 //Event Listener
-document.getElementById("button").addEventListener("click", showOrder, calcCost);
+document.getElementById("button").addEventListener("click", showOrder);
